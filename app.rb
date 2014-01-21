3af7bf56344989c8d98d27db7d936fd4a705c36f
smarsh25 <sjm.atl@gmail.com>
2014-01-21 05:01:42 -0800

    got display product working, including showing category -used join

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ end
 
 # Get the form for creating a new product
 get '/products/new' do
-  # Get all rows from the categories table.
+  # Get all rows from the categories table, to provide as options in the form
   c = PGconn.new(:host => "localhost", :dbname => dbname)
   @categories = c.exec_params("SELECT id, name FROM categories;")
   c.close
@@ -56,7 +56,7 @@ post '/products' do
   # create a record in product_category table to show the relation to category
   if params["category_id"] != "none"
     c.exec_params("INSERT INTO product_category (product_id, category_id) VALUES ($1,$2)",
-                  [new_product_id, params["category_id"].to_i])
+                  [new_product_id, params["category_id"]])
   end
   
   c.close
@@ -70,6 +70,21 @@ post '/products/:id' do
   # Update the product.
   c.exec_params("UPDATE products SET (name, price, description) = ($2, $3, $4) WHERE products.id = $1 ",
                 [params["id"], params["name"], params["price"], params["description"]])
+
+  # IF a category was chosen...
+  # create a record in product_category table to show the relation to category
+
+  # Delete whatever category this product had
+  # (this accounts for setting to none, or changing to a new/different value)
+  c.exec_params("DELETE FROM product_category WHERE product_category.product_id = $1", [params["id"]])
+
+  # Save whatever category was selected (if it wasn't 'none')
+  if params["category_id"] != "none"
+    # save the
+    c.exec_params("INSERT INTO product_category (product_id, category_id) VALUES ($1,$2)",
+          [params["id"], params["category_id"].to_i])
+  end
+
   c.close
   redirect "/products/#{params["id"]}"
 end
@@ -77,6 +92,11 @@ end
 get '/products/:id/edit' do
   c = PGconn.new(:host => "localhost", :dbname => dbname)
   @product = c.exec_params("SELECT * FROM products WHERE products.id = $1", [params["id"]]).first
+
+  # get the products current category.
+  product_category_result = c.exec_params("SELECT category_id FROM product_category WHERE product_category.product_id = $1", [params["id"]]).first
+  product_category_result == nil ? @product_category = "0" : @product_category = product_category_result["category_id"]
+  # binding.pry
   @categories = c.exec_params("SELECT id, name FROM categories;")  
   c.close
   erb :edit_product
@@ -95,7 +115,13 @@ end
 # GET the show page for a particular product
 get '/products/:id' do
   c = PGconn.new(:host => "localhost", :dbname => dbname)
-  @product = c.exec_params("SELECT * FROM products WHERE products.id = $1;", [params[:id]]).first
+  # @product = c.exec_params("SELECT name, price, description FROM products AS p WHERE p.id = $1;", [params[:id]]).first
+
+  @product = c.exec_params("SELECT p.name, p.price, p.description, c.name AS c_name, c.id AS c_id FROM products AS p
+                              LEFT OUTER JOIN product_category AS pc on p.id = pc.product_id 
+                              LEFT OUTER JOIN categories AS c on pc.category_id = c.id 
+                              WHERE p.id = $1;", [params[:id]]).first
+
   c.close
   erb :product
 end
